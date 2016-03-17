8e66f457509ea5aa262f884bccfe394f90bbd3c8
MarvinDurot <marvin.dut@gmail.com>
2016-03-17 09:56:52 +0100

    Complete like card

--- dichtelj.php
+++ dichtelj.php
@@ -1,9 +1,20 @@
 <?php
 
 	require 'config.php';
-
 	dol_include_once('/core/lib/user.lib.php');
+	dol_include_once('/user/class/user.class.php');
+
+	$fk_user = GETPOST('id');
+
+	$object = new User($db);
+	$object->fetch($fk_user);
+
 	$head = user_prepare_head($object);
+	llxHeader();
+
 	dol_fiche_head($head, 'dichtelj', $title, 0, 'user');
 
+	dol_fiche_end();
+
+	llxFooter();
 ?>
